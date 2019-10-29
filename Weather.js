3d92207ca6ac7a2d708689c2497add0fbe85389c
mia <sansolmom@gmail.com>
2019-10-30 01:40:22 +0900

    fourth commit

--- Weather.js
+++ Weather.js
@@ -1,119 +1,8 @@
 import React from "react";
 import {View, Text, StyleSheet, StatusBar} from "react-native";
-import Proptypes from "prop-types";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
-import {LinearGradient} from "expo-linear-gradient";
-import {white} from "ansi-colors";
-
-const weatherOptions = {
-  Rain: {
-    colors: ["#00C6FB", "#005BEA"],
-    title: "Raining like a MF",
-    subtitle: "For more info look outside",
-    icon: "weather-rainy"
-  },
-  Clear: {
-    colors: ["#FEF253", "#FF7300"],
-    title: "Sunny as fuck",
-    subtitle: "Go get your ass burnt",
-    icon: "weather-sunny"
-  },
-  Thunderstorm: {
-    colors: ["#00ECBC", "#007ADF"],
-    title: "Thunderstorm in the house",
-    subtitle: "Actually, outside of the house",
-    icon: "weather-lightning"
-  },
-  Clouds: {
-    colors: ["#D7D2CC", "#304352"],
-    title: "Clouds",
-    subtitle: "I know, fucking boring",
-    icon: "weather-cloudy"
-  },
-  Snow: {
-    colors: ["#7DE2FC", "#B9B6E5"],
-    title: "Cold as balls",
-    subtitle: "Do you want to build a snowman? Fuck no.",
-    icon: "weather-snowy"
-  },
-  Drizzle: {
-    colors: ["#89F7FE", "#66A6FF"],
-    title: "Drizzle",
-    subtitle: "Is like rain, but gay 🏳️‍🌈",
-    icon: "weather-hail"
-  },
-  Haze: {
-    colors: ["#89F7FE", "#66A6FF"],
-    title: "Haze",
-    subtitle: "Don't know what that is 💩",
-    icon: "weather-hail"
-  },
-  Mist: {
-    colors: ["#D7D2CC", "#304352"],
-    title: "Mist!",
-    subtitle: "It's like you have no glasses on.",
-    icon: "weather-fog"
-  }
-};
-
-export default function Weather({temp, condition}) {
-  return (
-    <LinearGradient
-      colors={weatherOptions[condition].colors}
-      style={styles.container}
-    >
-      <StatusBar barStyle="light-content" />
-      <View style={styles.halfContainer}>
-        <MaterialCommunityIcons
-          name={weatherOptions[condition].iconName}
-          size={100}
-          color="white"
-        />
-        <Text style={styles.temp}>{temp}o</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <View>
-          <Text style={styles.title}>{weatherOptions[condition].title}</Text>
-          <Text style={styles.subtitle}>
-            {weatherOptions[condition].subtitle}
-          </Text>
-        </View>
-      </View>
-    </LinearGradient>
-  );
-}
-
-Weather.propTypes = {
-  temp: Proptypes.number.isRequired,
-  condition: Proptypes.oneOf([
-    "Thuntherstorm",
-    "Drizzle",
-    "Rain",
-    "Snow",
-    "Atomoshere",
-    "Clear",
-    "Clouds",
-    "Haze",
-    "Mist",
-    "Dust"
-  ]).isRequired
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  temp: {
-    fontSize: 36,
-    color: "white"
-  },
-  halfContainer: {import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
-import { LinearGradient } from "expo-linear-gradient";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {LinearGradient} from "expo-linear-gradient";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 const weatherOptions = {
   Thunderstorm: {
@@ -176,7 +65,7 @@ const weatherOptions = {
   }
 };
 
-export default function Weather({ temp, condition }) {
+export default function Weather({temp, condition}) {
   return (
     <LinearGradient
       colors={weatherOptions[condition].gradient}
@@ -248,16 +137,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     justifyContent: "center",
     flex: 1
-  }
-});
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  title: {color: white, fontSize: 44, marginBottom: 10},
-  subtitle: {fontWeight: "600", fontSize: 24, color: white},
-  textContainer: {
-    paddingVertical: 10,
-    alignItems: "flex-start"
   }
 });
